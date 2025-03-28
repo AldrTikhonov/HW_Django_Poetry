cf09fcb431139cf47a6eaf11bcce9692807b3c70
Alexander Tikhonov <AldrTikhonov>
2025-03-28 11:34:19 +0300

    eighth commit

--- catalog/models.py
+++ catalog/models.py
@@ -6,13 +6,13 @@ class Category(models.Model):
     name = models.CharField(
         max_length=100,
         verbose_name="Наименование категории",
-        help_text="Введите наименование категории",
+        #help_text="Введите наименование категории",
     )
     description = models.TextField(
         blank=True,
         null=True,
         verbose_name="Описание",
-        help_text="Введите описание категории",
+        #help_text="Введите описание категории",
     )
 
     class Meta:
@@ -28,26 +28,26 @@ class Product(models.Model):
     name = models.CharField(
         max_length=100,
         verbose_name="Наименование продукта",
-        help_text="Введите наименование продукта",
+        #help_text="Введите наименование продукта",
     )
     description = models.TextField(
         blank=True,
         null=True,
         verbose_name="Описание",
-        help_text="Введите описание продукта",
+        #help_text="Введите описание продукта",
     )
     image = models.ImageField(
         upload_to="Product/image",
         blank=True,
         null=True,
         verbose_name="Изображение",
-        help_text="Загрузите изображение продукта",
+        #help_text="Загрузите изображение продукта",
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         verbose_name="Категория продукта",
-        help_text="Введите категорию продукта",
+        #help_text="Введите категорию продукта",
         blank=True,
         null=True,
         related_name="Products",
@@ -56,22 +56,22 @@ class Product(models.Model):
         max_digits=10,
         decimal_places=2,
         verbose_name="Цена продукта",
-        help_text="Введите цену продукта",
+        #help_text="Введите цену продукта",
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата создания",
-        help_text="Введите дату создания",
+        #help_text="Введите дату создания",
     )
     updated_at = models.DateTimeField(
         auto_now=True,
         verbose_name="Дата последнего изменения",
-        help_text="Введите дату последнего изменения",
+        #help_text="Введите дату последнего изменения",
     )
 
     views_counter = models.PositiveIntegerField(
         verbose_name="счетчик просмотров",
-        help_text="Укажите количество просмотров",
+        #help_text="Укажите количество просмотров",
         default=0,
     )
 
